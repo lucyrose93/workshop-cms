ea7bd4a86cb11e83bf6cf0dcd569215dd844bbc5
finnhodgkin <finnhodgkin@gmail.com>
2017-03-13 15:04:51 +0000

    serve public files from server

--- server.js
+++ server.js
@@ -12,9 +12,30 @@ function handler (req, resp) {
   var endpoint = req.url;
 
     if (endpoint==="/"){
+
       resp.writeHead(200, {"Content-Type": "text/html"});
 
       fs.readFile(__dirname+'/public/index.html', (err, file) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        resp.end(file);
+      });
+
+    } else {
+      console.log(endpoint);
+      var contentType = {
+        'html':'text/html',
+        'css':'text/css',
+        'js':'application/javascript',
+        'ico':'image/x-icon'
+      };
+      var extension = endpoint.split('.');
+
+      resp.writeHead(200, {'Content-Type': contentType[extension]});
+
+      fs.readFile(__dirname + '/public' + endpoint, (err, file) => {
         if (err) {
           console.log(err);
           return;
