799b121f976c7b6362c01b0d23ad64f3e844d8ce
finnhodgkin <finnhodgkin@gmail.com>
2017-03-13 14:36:05 +0000

    add handler

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var http = require('http');
-var server = http.createServer();
+var server = http.createServer(handler);
+
+var message = 'I am so happy to be part of the Node Girls workshop!';
+
+function handler (req, resp) {
+  resp.writeHead(200, {"Content-Type": "text/html"});
+  resp.write(message);
+  resp.end();
+}
 
 server.listen(3000, function () {
-
     console.log("Server is listening on port 3000.  Ready to accept requests!");
 });
