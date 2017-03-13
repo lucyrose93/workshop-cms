5e5a529c96ea3e1b40ee94c42212460a48a8ec10
finnhodgkin <finnhodgkin@gmail.com>
2017-03-13 16:10:11 +0000

    completed step 6 (pushing the form)

--- server.js
+++ server.js
@@ -1,72 +1,14 @@
 
 
 var http = require('http');
-var fs = require('fs');
-var querystring = require("querystring");
+
+var handler = require('./src/handler');
+// var fs = require('fs');
+// var querystring = require("querystring");
 
 
 
 var server = http.createServer(handler);
-
-var message = 'I am so happy to be part of the Node Girls workshop!';
-
-function handler (req, resp) {
-
-  var method = req.method;
-  var endpoint = req.url;
-
-    if (endpoint==="/"){
-
-      resp.writeHead(200, {"Content-Type": "text/html"});
-
-      fs.readFile(__dirname+'/public/index.html', (err, file) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        resp.end(file);
-      });
-
-
-    } else {
-      var contentType = {
-        'html':'text/html',
-        'css':'text/css',
-        'js':'application/javascript',
-        'ico':'image/x-icon'
-      };
-      var extension = endpoint.split('.');
-
-      resp.writeHead(200, {'Content-Type': contentType[extension]});
-
-      fs.readFile(__dirname + '/public' + endpoint, (err, file) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        resp.end(file);
-      });
-
-      var allTheData = '';
-        req.on('data', function (chunkOfData) {
-
-          allTheData += chunkOfData;
-      });
-
-      req.on('end', function () {
-
-        var convertedData = querystring.parse(allTheData);
-        console.log(convertedData);
-
-        resp.writeHead(205, {'Location': '/'});
-
-        //resp.end()
-
-      });
-
-    }
-  }
-
 
 server.listen(3000, function () {
     console.log("Server is listening on port 3000.  Ready to accept requests!");
